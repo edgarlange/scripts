3ffb236f08894a7e6a8473887863166df23b905a
Edgar Lange <134360225+edgarlange@users.noreply.github.com>
2023-07-31 12:43:32 -0500

    Add files via upload

---
+++ post.ps1
@@ -0,0 +1,19 @@
+write-host "running init script"
+if(!(test-path -Path "c:\temp"))
+    {    
+        New-Item -ItemType directory -Path "C:\temp"
+        write-host "created temp directory"
+    }
+
+$URL = "https://github.com/microsoft/AzureStorageExplorer/releases/download/v1.30.2/StorageExplorer-windows-x64.exe"
+$Path="c:\temp\StorageExplorer-windows-x64.exe"
+Invoke-WebRequest -URI $URL -OutFile $Path
+
+$URL2 = "https://aka.ms/ssmsfullsetup"
+$Path2="c:\temp\SSMS-Setup-ENU.exe"
+Invoke-WebRequest -URI $URL2 -OutFile $Path2
+
+$URL3 = "https://download.microsoft.com/download/8/8/0/880BCA75-79DD-466A-927D-1ABF1F5454B0/PBIDesktopSetup_x64.exe"
+$Path3="c:\temp\PBIDesktopSetup_x64.exe"
+Invoke-WebRequest -URI $URL3 -OutFile $Path3
+
