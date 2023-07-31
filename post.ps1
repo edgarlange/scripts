5bbeb645d781a25cead5f70ccb2175d2355e7244
Edgar Lange <134360225+edgarlange@users.noreply.github.com>
2023-07-31 13:21:24 -0500

    Update post.ps1

--- post.ps1
+++ post.ps1
@@ -17,3 +17,15 @@ $URL3 = "https://download.microsoft.com/download/8/8/0/880BCA75-79DD-466A-927D-1
 $Path3="c:\temp\PBIDesktopSetup_x64.exe"
 Invoke-WebRequest -URI $URL3 -OutFile $Path3
 
+$pathvargs = {c:\temp\StorageExplorer-windows-x64.exe /S /v/qn }
+Invoke-Command -ScriptBlock $pathvargs
+Start-Sleep 120
+
+$pathvargs1 = {c:\temp\SSMS-Setup-ENU.exe /S /v/qn }
+Invoke-Command -ScriptBlock $pathvargs1
+Start-Sleep 120
+
+$pathvargs2 = {c:\temp\PBIDesktopSetup_x64.exe /S /v/qn }
+Invoke-Command -ScriptBlock $pathvargs2
+Start-Sleep 120
+
