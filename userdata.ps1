2e42f5d5011b03ff99c4fe2e90bca27beda7a742
Edgar Lange <134360225+edgarlange@users.noreply.github.com>
2023-08-15 17:57:34 -0500

    Update userdata.ps1

--- userdata.ps1
+++ userdata.ps1
@@ -5,7 +5,7 @@ if(!(test-path -Path "c:\temp"))
         write-host "created temp directory"
     }
 
-$URL = "https://github.com/microsoft/AzureStorageExplorer/releases/download/v1.30.2/StorageExplorer-windows-x64.exe"
+$URL = "https://go.microsoft.com/fwlink/?linkid=2216182&clcid=0x40A"
 $Path="c:\temp\StorageExplorer-windows-x64.exe"
 Invoke-WebRequest -URI $URL -OutFile $Path
 
@@ -13,9 +13,9 @@ $URL2 = "https://aka.ms/ssmsfullsetup"
 $Path2="c:\temp\SSMS-Setup-ENU.exe"
 Invoke-WebRequest -URI $URL2 -OutFile $Path2
 
-$URL3 = "https://download.microsoft.com/download/8/8/0/880BCA75-79DD-466A-927D-1ABF1F5454B0/PBIDesktopSetup_x64.exe"
-$Path3="c:\temp\PBIDesktopSetup_x64.exe"
-Invoke-WebRequest -URI $URL3 -OutFile $Path3
+#$URL3 = "https://download.microsoft.com/download/8/8/0/880BCA75-79DD-466A-927D-1ABF1F5454B0/PBIDesktopSetup_x64.exe"
+#$Path3="c:\temp\PBIDesktopSetup_x64.exe"
+#Invoke-WebRequest -URI $URL3 -OutFile $Path3
 
 #$pathvargs = {c:\temp\StorageExplorer-windows-x64.exe /S /v/qn }
 #Invoke-Command -ScriptBlock $pathvargs
