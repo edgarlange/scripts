540ff9ac0e6560c60e8ff172774354213501a4fd
Edgar Lange <134360225+edgarlange@users.noreply.github.com>
2023-08-15 17:58:23 -0500

    Update userdata.ps1

--- userdata.ps1
+++ userdata.ps1
@@ -9,9 +9,9 @@ $URL = "https://go.microsoft.com/fwlink/?linkid=2216182&clcid=0x40A"
 $Path="c:\temp\StorageExplorer-windows-x64.exe"
 Invoke-WebRequest -URI $URL -OutFile $Path
 
-$URL2 = "https://aka.ms/ssmsfullsetup"
-$Path2="c:\temp\SSMS-Setup-ENU.exe"
-Invoke-WebRequest -URI $URL2 -OutFile $Path2
+#$URL2 = "https://aka.ms/ssmsfullsetup"
+#$Path2="c:\temp\SSMS-Setup-ENU.exe"
+#Invoke-WebRequest -URI $URL2 -OutFile $Path2
 
 #$URL3 = "https://download.microsoft.com/download/8/8/0/880BCA75-79DD-466A-927D-1ABF1F5454B0/PBIDesktopSetup_x64.exe"
 #$Path3="c:\temp\PBIDesktopSetup_x64.exe"
