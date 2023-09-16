ab2d1630c0b982a7b0b495df1fa546330b37d577
Edgar Lange <134360225+edgarlange@users.noreply.github.com>
2023-09-15 19:33:25 -0500

    Update userdata.ps1

--- userdata.ps1
+++ userdata.ps1
@@ -9,16 +9,16 @@ $URL = "https://go.microsoft.com/fwlink/?linkid=2216182&clcid=0x40A"
 $Path="c:\temp\StorageExplorer-windows-x64.exe"
 Invoke-WebRequest -URI $URL -OutFile $Path
 
-$pathvargs = {c:\temp\StorageExplorer-windows-x64.exe /S /v/qn }
-Invoke-Command -ScriptBlock $pathvargs
+#$pathvargs = {c:\temp\StorageExplorer-windows-x64.exe /S /v/qn }
+#Invoke-Command -ScriptBlock $pathvargs
 
-#$URL2 = "https://aka.ms/ssmsfullsetup"
-#$Path2="c:\temp\SSMS-Setup-ENU.exe"
-#Invoke-WebRequest -URI $URL2 -OutFile $Path2
+$URL2 = "https://aka.ms/ssmsfullsetup"
+$Path2="c:\temp\SSMS-Setup-ENU.exe"
+Invoke-WebRequest -URI $URL2 -OutFile $Path2
 
-#$URL3 = "https://download.microsoft.com/download/8/8/0/880BCA75-79DD-466A-927D-1ABF1F5454B0/PBIDesktopSetup_x64.exe"
-#$Path3="c:\temp\PBIDesktopSetup_x64.exe"
-#Invoke-WebRequest -URI $URL3 -OutFile $Path3
+$URL3 = "https://download.microsoft.com/download/8/8/0/880BCA75-79DD-466A-927D-1ABF1F5454B0/PBIDesktopSetup_x64.exe"
+$Path3="c:\temp\PBIDesktopSetup_x64.exe"
+Invoke-WebRequest -URI $URL3 -OutFile $Path3
 
 #$pathvargs1 = {c:\temp\SSMS-Setup-ENU.exe /S /v/qn }
 #Invoke-Command -ScriptBlock $pathvargs1
